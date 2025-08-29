c16a775bb2700be60610b52c9082f47d9a33fcd8
nw-ctrl <nextwave@outlook.com.au>
2025-08-29 16:51:57 +1000

    srcconfigupdated

--- frontend/src/config.js
+++ frontend/src/config.js
@@ -1,5 +1,2 @@
-// This URL will be replaced later with your live Render backend 
-URL.
-export const API_BASE_URL = 
-'https://bztradwave-api.onrender.com';
-
+// The URL for the live Render backend API
+export const API_BASE_URL = 'https://bztradewave-api.onrender.com';
